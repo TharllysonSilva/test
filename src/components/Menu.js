939f5c102a353cce0193b1621035c056e567a473
Tarllysson Silva <44323993+TharllysonSilva@users.noreply.github.com>
2023-03-24 02:17:45 -0300

    Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,26 @@ function Menu ({active, setActive, setCategory}) {
         {id: 6, name: "Esportes", value: "esportes"},
         {id: 7, name: "Tecnologia", value: "tecnologia"},
     ]
+
+    function onClick (id, value){
+        setActive(id)
+        setCategory(value)
+    }
+    return (
+        <nav className="menu">
+            <ul>
+                {links.map(link => (
+                    <li 
+                    key={link.id}
+                    className={active === link.id ? "active" : "inactive"}
+                    onClick={() => onClick(link.id, link.value)}
+                    >
+                        
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
 
 export default Menu
