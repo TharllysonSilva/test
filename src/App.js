243853071d254abfed70bc4a836527b6420d7b23
Tarllysson Silva <44323993+TharllysonSilva@users.noreply.github.com>
2023-03-24 01:18:14 -0300

    App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Menu from './components/Menu'
 import NewsGrid from './components/NewsGrid'
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([])
+  const [active, setActive] = useState([1])
+  const [category, setCategory] = useState("general")
 
+  useEffect(() => {
+    fetch('https:/newsapi.org/v2/top-headlines?country=us&category=${category}&apikey=021baff830cb4c8fa2c688eefdd78b46')
+    .then(res => res.json())
+    .then(data => setItems(data.articles))
+  })
   return (
     <div className="App">
       <h1 className="title">Veja as últimas notícias</h1>
