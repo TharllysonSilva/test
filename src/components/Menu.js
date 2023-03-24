a037680711271fa638508fe9f760cc91be8ce7cc
Tarllysson Silva <44323993+TharllysonSilva@users.noreply.github.com>
2023-03-24 02:12:40 -0300

    Menu

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+function Menu ({active, setActive, setCategory}) {
+    const links = [
+        {id: 1, name: "Geral", value: "geral"},
+        {id: 2, name: "Negócios", value: "negocios"},
+        {id: 3, name: "Entreterimento", value: "Entreterimento"},
+        {id: 4, name: "Saúde", value: "saude"},
+        {id: 5, name: "Ciência", value: "ciencia"},
+        {id: 6, name: "Esportes", value: "esportes"},
+        {id: 7, name: "Tecnologia", value: "tecnologia"},
+    ]
+}
+
+export default Menu
