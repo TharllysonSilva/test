7e56e663b9b04cc564d237b32f0d6401ba6f56cd
Tarllysson Silva <44323993+TharllysonSilva@users.noreply.github.com>
2023-03-24 01:11:06 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import Menu from './components/Menu'
+import NewsGrid from './components/NewsGrid'
 
 function App() {
+  const [items, setItems] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Veja as últimas notícias</h1>
+      <Menu/>
+      <NewsGrid/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
